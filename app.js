3f95cb8e3820e82ffaf203b671ad0ce0905c2086
fertejj <fernandojtejerina@gmail.com>
2025-01-27 01:07:22 -0300

    Filtra x genero pasados x query

--- app.js
+++ app.js
@@ -9,13 +9,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
-  res.json(movies);
+  // res.json(movies);
+  const { genre } = req.query;
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
 });
 
-app.get("/movies/:id", (req, res) => { // path-to-regexp
+app.get("/movies/:id", (req, res) => {
+  // path-to-regexp
   const { id } = req.params;
   const movie = movies.find((movie) => movie.id === id);
-  if(movie) return res.json(movie);
+  if (movie) return res.json(movie);
   res.status(404).json({ message: "Movie not found" });
 });
 PORT = process.env.PORT ?? 3000;
